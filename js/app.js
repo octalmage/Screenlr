81ebe78099cfb7d9de46c329d4f0be1562a2c287
Jason Stallings <jason@stallin.gs>
2014-12-31 12:02:42 -0600

    Got ccreencaptures working!
    Command+Shift+T is the hotkey.

---
+++ js/app.js
@@ -0,0 +1,50 @@
+var gui = require('nw.gui');
+var win = gui.Window.get();
+
+var exec = require('child_process').exec;
+
+var nativeMenuBar = new gui.Menu({ type: "menubar" });
+nativeMenuBar.createMacBuiltin("Screenlr");
+win.menu = nativeMenuBar;
+
+// Create a tray text
+var tray = new gui.Tray({ title: 'Screenlr'});
+
+// Give it a menu.
+var menu = new gui.Menu();
+menu.append(new gui.MenuItem({label: 'Exit', click: function() 
+{
+    gui.App.quit();
+}, }));
+tray.menu = menu;
+
+//Hotkey
+var option = 
+{
+  key : "Ctrl+Shift+T",
+  active : function() 
+  {
+  	screenshot()
+  },
+  failed : function(msg) 
+  {
+    console.log(msg);
+  }
+};
+
+var shortcut = new gui.Shortcut(option);
+
+gui.App.registerGlobalHotKey(shortcut);
+
+function screenshot()
+{
+	exec('/usr/sbin/screencapture -i temp.png', function(error, stdout, stderr) 
+	{
+		console.log(stdout);
+		console.log(error)
+    	if (error == null) 
+    	{
+        	$("#capture").css("background-image", "url('temp.png?" + new Date().getTime() + "')");
+    	}
+	});	
+}
