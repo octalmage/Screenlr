8932e13519186d8c84e950e8e7d3c6e526755530
Jason Stallings <jason@stallin.gs>
2015-01-29 10:24:39 -0600

    Fixed code style.

--- js/app.js
+++ js/app.js
@@ -3,33 +3,42 @@ var win = gui.Window.get();
 
 var exec = require('child_process').exec;
 
-var nativeMenuBar = new gui.Menu({ type: "menubar" });
+var nativeMenuBar = new gui.Menu(
+{
+	type: "menubar"
+});
 nativeMenuBar.createMacBuiltin("Screenlr");
 win.menu = nativeMenuBar;
 
 // Create a tray text
-var tray = new gui.Tray({ title: 'Screenlr'});
+var tray = new gui.Tray(
+{
+	title: 'Screenlr'
+});
 
 // Give it a menu.
 var menu = new gui.Menu();
-menu.append(new gui.MenuItem({label: 'Exit', click: function() 
+menu.append(new gui.MenuItem(
 {
-    gui.App.quit();
-}, }));
+	label: 'Exit',
+	click: function()
+	{
+		gui.App.quit();
+	},
+}));
 tray.menu = menu;
 
 //Hotkey
-var option = 
-{
-  key : "Ctrl+Shift+T",
-  active : function() 
-  {
-  	screenshot()
-  },
-  failed : function(msg) 
-  {
-    console.log(msg);
-  }
+var option = {
+	key: "Ctrl+Shift+T",
+	active: function()
+	{
+		screenshot()
+	},
+	failed: function(msg)
+	{
+		console.log(msg);
+	}
 };
 
 var shortcut = new gui.Shortcut(option);
@@ -38,21 +47,21 @@ gui.App.registerGlobalHotKey(shortcut);
 
 $(function()
 {
-    $("#submitButton").on("click", function submitClicked()
-    {
+	$("#submitButton").on("click", function submitClicked()
+	{
 
-    });
+	});
 });
 
 function screenshot()
 {
-	exec('/usr/sbin/screencapture -i temp.png', function(error, stdout, stderr) 
+	exec('/usr/sbin/screencapture -i temp.png', function(error, stdout, stderr)
 	{
 		console.log(stdout);
 		console.log(error)
-    	if (error == null) 
-    	{
-        	$("#capture").css("background-image", "url('temp.png?" + new Date().getTime() + "')");
-    	}
-	});	
+		if (error == null)
+		{
+			$("#capture").css("background-image", "url('temp.png?" + new Date().getTime() + "')");
+		}
+	});
 }
